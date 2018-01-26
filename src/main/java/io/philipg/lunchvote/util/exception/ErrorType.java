c516ad88caaf8a25220869a6e1c5a22b01158040
Philip Goldman <philip.goldm@gmail.com>
2018-01-26 18:32:57 +0200

    Added exception handlers

---
+++ src/main/java/io/philipg/lunchvote/util/exception/ErrorType.java
@@ -0,0 +1,8 @@
+package io.philipg.lunchvote.util.exception;
+
+public enum ErrorType {
+    APP_ERROR,
+    DATA_NOT_FOUND,
+    DATA_ERROR,
+    VALIDATION_ERROR
+}
---
+++ src/main/java/io/philipg/lunchvote/web/ExceptionInfoHandler.java
@@ -0,0 +1,68 @@
+package io.philipg.lunchvote.web;
+
+import io.philipg.lunchvote.util.ValidationUtil;
+import io.philipg.lunchvote.util.exception.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static io.philipg.lunchvote.util.exception.ErrorType.*;
+
+@RestControllerAdvice(annotations = RestController.class)
+@Order(Ordered.HIGHEST_PRECEDENCE + 5)
+public class ExceptionInfoHandler {
+    private static Logger log = LoggerFactory.getLogger(ExceptionInfoHandler.class);
+
+    //  http://stackoverflow.com/a/22358422/548473
+    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(NotFoundException.class)
+    public ErrorInfo handleError(HttpServletRequest req, NotFoundException e) {
+        return logAndGetErrorInfo(req, e, false, DATA_NOT_FOUND);
+    }
+
+    @ResponseStatus(value = HttpStatus.CONFLICT)  // 409
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ErrorInfo conflict(HttpServletRequest req, DataIntegrityViolationException e) {
+        return logAndGetErrorInfo(req, e, true, DATA_ERROR);
+    }
+
+    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)  // 422
+    @ExceptionHandler({BindException.class, MethodArgumentNotValidException.class})
+    public ErrorInfo bindValidationError(HttpServletRequest req, Exception e) {
+        BindingResult result = e instanceof BindException ?
+                ((BindException) e).getBindingResult() : ((MethodArgumentNotValidException) e).getBindingResult();
+
+        String[] details = result.getFieldErrors().stream()
+                .map(fe -> {
+                    String msg = fe.getDefaultMessage();
+                    return (msg.startsWith(fe.getField())) ? msg : fe.getField() + ' ' + msg;
+                })
+                .toArray(String[]::new);
+
+        return logAndGetErrorInfo(req, e, false, VALIDATION_ERROR, details);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorInfo handleError(HttpServletRequest req, Exception e) {
+        return logAndGetErrorInfo(req, e, true, APP_ERROR);
+    }
+
+    private static ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception e, boolean logException, ErrorType errorType, String... details) {
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        if (logException) {
+            log.error(errorType + " at request " + req.getRequestURL(), rootCause);
+        } else {
+            log.warn("{} at request  {}: {}", errorType, req.getRequestURL(), rootCause.toString());
+        }
+        return new ErrorInfo(req.getRequestURL(), errorType, details.length != 0 ? details : new String[]{rootCause.toString()});
+    }
+}
